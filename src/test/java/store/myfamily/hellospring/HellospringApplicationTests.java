3c40b80f052abdac5a687575a7be011fb7a50660
Dong-Hyeok-Ing <kdhhdk83@gmail.com>
2024-06-26 20:28:27 +0900

    토비 Spring 6
    first commit

---
+++ src/test/java/store/myfamily/hellospring/HellospringApplicationTests.java
@@ -0,0 +1,13 @@
+package store.myfamily.hellospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellospringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
