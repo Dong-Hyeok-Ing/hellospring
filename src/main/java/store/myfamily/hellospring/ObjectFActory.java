1259b98d467f4ca896e180dbc3599a4c948b3a5e
Dong-Hyeok-Ing <kdhhdk83@gmail.com>
2024-07-11 08:53:34 +0900

    스프링으로 하나씩
    이제부터 시작이다
    현재 코드를 응용해서 무궁무진하게 확장되고 응용된다.
    IOC/DI가 어떻게 적용되는지 이해해야 한다.

--- src/main/java/store/myfamily/hellospring/ObjectFActory.java
+++ src/main/java/store/myfamily/hellospring/ObjectFActory.java
@@ -1,11 +1,17 @@
 package store.myfamily.hellospring;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ObjectFActory {
 
+    @Bean
     public PaymentService PaymentService(){
         return new PaymentService(getExRateProvider());
     }
 
+    @Bean
     public ExRateProvider getExRateProvider(){
         return new WebApiExRateProvider();
     }
